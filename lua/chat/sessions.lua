44ac3ae4e134d2e2a2c5b254aed111041e4f6028
Eric Wong <wsdjeg@outlook.com>
2026-02-03 01:40:31 +0800

    feat: add sessions support

---
+++ lua/chat/sessions.lua
@@ -0,0 +1,20 @@
+local sessions = {}
+
+---@class chat.session
+---@field id string
+---@field system_prompt string
+---@field messages table
+
+function sessions.write_cache(session)
+end
+
+function sessions.load_cache()
+end
+
+function sessions.get()
+end
+
+function sessions.gen_id()
+end
+
+return sessions
