c65700642317ab8c26937fa0f5438475d77356f2
Eric Wong <wsdjeg@outlook.com>
2026-02-03 20:19:57 +0800

    feat: add picker chat_provider source

--- lua/picker/sources/chat_provider.lua
+++ lua/picker/sources/chat_provider.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local previewer = require('picker.previewer.buffer')
+
 function M.get()
   return vim.tbl_map(function(t)
     t = vim.fn.fnamemodify(t, ':t:r')
@@ -22,6 +24,26 @@ function M.default_action(item)
       require('chat.windows').set_model(available_models[1])
     end
   end
+end
+
+M.preview_win = true
+
+function M.preview(item, win, buf)
+  local ok, provider = pcall(require, 'chat.providers.' .. item.value)
+  if ok then
+    local available_models = provider.available_models()
+    if #available_models > 0 then
+      previewer.buflines = {
+        '## Available models', ''
+      }
+      for _, model in ipairs(available_models) do
+        table.insert(previewer.buflines, '- ' .. model)
+      end
+    end
+  end
+  local line = 1
+  previewer.filetype = 'markdown'
+  previewer.preview(line, win, buf, true)
 end
 
 return M
