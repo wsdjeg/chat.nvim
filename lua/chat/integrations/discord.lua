0d8543e054102c7dce417be8d85c672c434f31e3
Eric Wong <wsdjeg@outlook.com>
2026-03-07 00:41:09 +0800

    update discord config

--- lua/chat/integrations/discord.lua
+++ lua/chat/integrations/discord.lua
@@ -1,37 +1,46 @@
 local M = {}
 
 local job = require('job')
-
-local config = {
-  token = nil,
-  channel_id = nil,
-}
+local config = require('chat.config')
 
 function M.setup(opts)
   config = vim.tbl_deep_extend('force', config, opts or {})
 end
 
 function M.send_message(content)
-  if not config.token or not config.token then
+  if
+    not config.config.integrations.discord.channel_id
+    or not not config.config.integrations.discord.token
+  then
     return
   end
   local cmd = {
     'curl',
-    '-X',
-    'POST',
-    'https://discord.com/api/v10/channels/' .. config.channel_id .. '/messages',
+    '-s',
+    'https://discord.com/api/v10/channels/'
+      .. config.channel_id
+      .. '/messages',
     '-H',
     'Authorization: Bot ' .. config.token,
     '-H',
     'Content-Type: application/json',
+    '-X',
+    'POST',
     '-d',
-    vim.json.encode({
-      content = 'hello',
-    }),
+    '@-',
   }
+  local jobid = job.start(cmd, {
+    on_exit = function(id, code, single) end,
+  })
+  job.send(
+    jobid,
+    vim.json.encode({
+      content = content,
+    })
+  )
+  job.send(jobid, nil)
 end
 
-function M.receive_messages(callback)
-end
+function M.receive_messages(callback) end
 
 return M
