bae68baa8e64a9563b98c7007e1e2cd6bdf888ef
Eric Wong <wsdjeg@outlook.com>
2026-03-07 02:37:55 +0800

    stop send message

--- lua/chat/sessions.lua
+++ lua/chat/sessions.lua
@@ -568,7 +568,7 @@ end
 
 function M.append_message(session, message)
   if message.role == 'assistant' and message.content then
-    require('chat.integrations.discord').send_message(message.content)
+    -- require('chat.integrations.discord').send_message(message.content)
   end
   table.insert(sessions[session].messages, message)
 end
